2451969adc1523a31449d7322d214c52436a7c6b
Niiazbek Mamasaliev <niyazbek.mamasaliev@gmail.com>
2023-04-07 05:20:19 +0200

    fetching from mock API

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ListItem = ({ note }) => {
-  console.log("PROPS: ", note);
+  // console.log("PROPS: ", note);
   return (
     <Link to={`/note/${note.id}`}>
       <div className="notes-list-item">
