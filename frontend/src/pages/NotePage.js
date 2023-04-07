6b6b83ae6f593dd0ca1159d8aff3fe42445ab812
Niiazbek Mamasaliev <niyazbek.mamasaliev@gmail.com>
2023-04-07 05:59:41 +0200

    fetching the singleNote is not working

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,29 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-import notes from "../assets/data";
 import { Link } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
 const NotePage = (props) => {
+  let [note, setNote] = useState(null);
+
   let { id } = useParams();
+  console.log(id);
+  // let id = 1;
   // console.log("params:", id);
 
-  let note = notes.find((note) => note.id === Number(id)); //Number() to ensure that the id is numeric
+  // let note = notes.find((note) => note.id === Number(id)); //Number() to ensure that the id is numeric
 
-  return (
-    <div className="note">
-      <div className="note-header">
-        <h3>
-          <Link to="/">
-            <ArrowLeft />
-          </Link>
-        </h3>
+  useEffect(() => {
+    getNote();
+  }, [id]);
+
+  let getNote = async () => {
+    let response = await fetch`(http://localhost:8000/notes/${id})`;
+
+    let data = await response.json();
+    setNote(data);
+
+    return (
+      <div className="note">
+        <div className="note-header">
+          <h3>
+            <Link to="/">
+              <ArrowLeft />
+            </Link>
+          </h3>
+        </div>
+        {/* <p>{note?.body}</p> */}
+        <textarea value={note?.body}></textarea>
       </div>
-      {/* <p>{note?.body}</p> */}
-      <textarea value={note?.body}></textarea>
-    </div>
-  );
+    );
+  };
 };
 
 export default NotePage;
